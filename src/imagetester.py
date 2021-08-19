2afbdc4689b074ba8239756b997e235c6597cc88
vinh.ngo <vinh.ngo@eyeq.tech>
2021-08-19 13:42:40 +0700

    new update

---
+++ src/imagetester.py
@@ -0,0 +1,46 @@
+import os
+import math
+
+import utility
+from data import common
+
+import torch
+import cv2
+
+from tqdm import tqdm
+
+class ImageTester():
+    def __init__(self, args, my_model, ckp):
+        self.args = args
+        self.scale = args.scale
+
+        self.ckp = ckp
+        self.model = my_model
+
+        self.filename, _ = os.path.splitext(os.path.basename(args.dir_demo))
+
+    def test(self):
+        torch.set_grad_enabled(False)
+
+        self.ckp.write_log('\nEvaluation on single image:')
+        self.model.eval()
+
+        timer_test = utility.timer()
+        for idx_scale, scale in enumerate(self.scale):
+            img = cv2.imread(self.args.dir_demo)
+            lr, = common.set_channel(lr, n_channels=self.args.n_colors)
+            lr, = common.np2Tensor(lr, rgb_range=self.args.rgb_range)
+            lr, = self.prepare(lr.unsqueeze(0))
+            sr = self.model(lr, idx_scale)
+            sr = utility.quantize(sr, self.args.rgb_range).squeeze(0)
+            normalized = sr * 255 / self.args.rgb_range
+            ndarr = normalized.byte().permute(1, 2, 0).cpu().numpy()
+            cv2.imwrite('../../upscaled.jpg',ndarr)
+
+    def prepare(self, *args):
+        device = torch.device('cpu' if self.args.cpu else 'cuda')
+        def _prepare(tensor):
+            if self.args.precision == 'half': tensor = tensor.half()
+            return tensor.to(device)
+
+        return [_prepare(a) for a in args]
--- src/main.py
+++ src/main.py
@@ -13,21 +13,27 @@ checkpoint = utility.checkpoint(args)
 def main():
     global model
     if args.data_test == ['video']:
-        from videotester import VideoTester
+        # i've modifed so that video could be image
+        from imagetester import ImageTester
         model = model.Model(args, checkpoint)
-        t = VideoTester(args, model, checkpoint)
+        t = ImageTester(args, model, checkpoint)
         t.test()
-    else:
-        if checkpoint.ok:
-            loader = data.Data(args)
-            _model = model.Model(args, checkpoint)
-            _loss = loss.Loss(args, checkpoint) if not args.test_only else None
-            t = Trainer(args, loader, _model, _loss, checkpoint)
-            while not t.terminate():
-                t.train()
-                t.test()
+    # if args.data_test == ['video']:
+    #     from videotester import VideoTester
+    #     model = model.Model(args, checkpoint)
+    #     t = VideoTester(args, model, checkpoint)
+    #     t.test()
+    # else:
+    #     if checkpoint.ok:
+    #         loader = data.Data(args)
+    #         _model = model.Model(args, checkpoint)
+    #         _loss = loss.Loss(args, checkpoint) if not args.test_only else None
+    #         t = Trainer(args, loader, _model, _loss, checkpoint)
+    #         while not t.terminate():
+    #             t.train()
+    #             t.test()
 
-            checkpoint.done()
+    #         checkpoint.done()
 
 if __name__ == '__main__':
     main()
